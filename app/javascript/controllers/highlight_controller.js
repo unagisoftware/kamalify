560bcf046bb7cfb409f46712457f270054d3628c
Agustin Serena <aserena92@gmail.com>
2025-02-27 13:07:30 -0300

    import yaml language only

--- app/javascript/controllers/highlight_controller.js
+++ app/javascript/controllers/highlight_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus";
-import hljs from "highlight.js"
+import hljs from "highlight-js"
+import yaml from "highlight-yaml";
 
+hljs.registerLanguage("yaml", yaml);
 
 export default class extends Controller {
   connect() {
