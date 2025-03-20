6480e677e65b85c8bf81778dd4a980757964d6e5
Clara Barelli <clara.barelli@unagi.com.ar>
2025-03-20 08:26:53 -0300

    Deleted old classes

--- app/controllers/yaml_previews_controller.rb
+++ app/controllers/yaml_previews_controller.rb
@@ -2,6 +2,18 @@
 
 class YamlPreviewsController < ApplicationController
   def create
-    @yaml_document = YamlDocument.new(params)
+    @yaml_document = YamlDocument.new(yaml_params)
+  end
+
+  private
+
+  def yaml_params
+    params.permit(
+      :name, 
+      :docker_image, 
+      :arch,
+      registry: [:username, :password],
+      servers: [:host, { jobs: [:host, :cmd] }]
+    )
   end
 end
