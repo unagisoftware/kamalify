20150a127d3fb9514ead9f4f3c93b7a48e5ab233
Nicolás Galdámez <nicolas.galdamez@unagisoftware.com>
2025-05-07 16:42:37 -0300

    Add environment variables block

--- app/models/env_config.rb
+++ app/models/env_config.rb
@@ -4,10 +4,33 @@ class EnvConfig < BaseConfig
   private
 
   def values
-    {}
+    {
+      clear:,
+      secret:
+    }
+  end
+
+  def clear
+    return if @params[:clear].blank?
+
+    @params[:clear].each_with_object({}) do |item, hash|
+      next if item.exclude?(":")
+
+      key, value = item.split(":", 2)
+      hash[key.strip] = value.strip
+    end
+  end
+
+  def secret
+    items = @params[:secret]
+
+    items.presence && Array.wrap(items.compact_blank)
+  end
+
+  def vars_array(items)
   end
 
   def defaults
-    { secrets: [ "RAILS_MASTER_KEY" ] }
+    { secret: [ "RAILS_MASTER_KEY" ] }
   end
 end
