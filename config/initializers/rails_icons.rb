4aec9d8f81283e06730f4ffcec1ea82829eb706c
Nicolás Galdámez <nicolas.galdamez@unagisoftware.com>
2025-05-07 14:41:18 -0300

    Changes icons from feather to heroicons

--- config/initializers/rails_icons.rb
+++ config/initializers/rails_icons.rb
@@ -1,5 +1,5 @@
 RailsIcons.configure do |config|
-  config.default_library = "feather"
+  config.default_library = "heroicons"
   # config.default_variant = "" # Set a default variant for all libraries
 
   # Override Feather defaults
@@ -9,4 +9,21 @@ RailsIcons.configure do |config|
   # config.libraries.feather.default.css = "size-6"
   # config.libraries.feather.default.stroke_width = "2"
   # config.libraries.feather.default.data = {}
+
+  # Override Heroicon defaults
+  # config.libraries.heroicons.default_variant = "" # Set a default variant for Heroicons
+  # config.libraries.heroicons.exclude_variants = [:mini, :micro] # Exclude specific variants
+
+  # config.libraries.heroicons.outline.default.css = "size-6"
+  # config.libraries.heroicons.outline.default.stroke_width = "1.5"
+  # config.libraries.heroicons.outline.default.data = {}
+
+  # config.libraries.heroicons.solid.default.css = "size-6"
+  # config.libraries.heroicons.solid.default.data = {}
+
+  # config.libraries.heroicons.mini.default.css = "size-5"
+  # config.libraries.heroicons.mini.default.data = {}
+
+  # config.libraries.heroicons.micro.default.css = "size-4"
+  # config.libraries.heroicons.micro.default.data = {}
 end
