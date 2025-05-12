b39b2ab566aa7bbb637d3487a739cf90282083e7
Nicolás Galdámez <nicolas.galdamez@unagisoftware.com>
2025-05-12 16:37:49 -0300

    Copy and download button

--- app/javascript/controllers/autosubmit_controller.js
+++ app/javascript/controllers/autosubmit_controller.js
@@ -1,16 +1,24 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["dynamicInput"];
+  static targets = ["format", "form", "dynamicInput"];
 
   dynamicInputTargetDisconnected() {
     this.submit();
   }
 
-  submit() {
+  submit({ params }) {
+    if (params.format === "yaml") {
+      this.formatTarget.value = "yaml";
+      this.formTarget.setAttribute("data-turbo", "false")
+    } else {
+      this.formatTarget.value = "html";
+      this.formTarget.setAttribute("data-turbo", "true")
+    }
+
     clearTimeout(this.timeout);
     this.timeout = setTimeout(() => {
-      this.element.requestSubmit();
+      this.formTarget.requestSubmit();
     }, 500); // Prevent excessive submissions
   }
 }
